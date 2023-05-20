0bb6725f20f33dc8fea25200b175f7cdcf256a35
Diwakar Mishra <105007661+RedC0der@users.noreply.github.com>
2023-05-20 10:30:58 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A[5];
+    
+    int i;
+    for ( i = 0; i < 5; i++)
+    {
+        cout<<"\n"<<A[i];
+    }
+    
+    return 0;
+}
